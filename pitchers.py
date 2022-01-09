1b37f9edf18c4d9747a4098ce5e37af8e56e2653
heuera <austinheuer24@gmail.com>
2022-01-09 16:25:57 -0500

    Analysis of 2021 starting pitcher stats. Includes all pitchers with > 100 IP.

--- pitchers.py
+++ pitchers.py
@@ -11,31 +11,63 @@ advanced = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/advanced.csv
 batted = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/battedball.csv")
 statcast = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/statcast.csv")
 
+########################################################################################################################
+##################################### DATA MANAGEMENT ##################################################################
+########################################################################################################################
+
 # Drop columns that are duplicated
 advanced = advanced.drop(['Name', 'Team', 'ERA'], axis=1)
 batted = batted.drop(['Name', 'Team', 'BABIP'], axis=1)
 statcast = statcast.drop(['Name', 'Team', 'IP', 'ERA'], axis=1)
+
+# Remove limit to number of columns pycharm displays
+pd.set_option('display.max_columns', None)
 
 # Merge datasets into a single dataframe
 pitchers_1 = pd.merge(standard, advanced, on='playerid')
 pitchers_2 = pd.merge(pitchers_1, batted, on='playerid')
 pitchers = pd.merge(pitchers_2, statcast, on='playerid')
 
+# Strip % from variabes with %
+per_cols = (
+"K%", "BB%", "K-BB%", "LOB%", "LD%", "GB%", "FB%", "IFFB%", "HR/FB", "Pull%", "Cent%", "Oppo%", "Soft%", "Med%",
+"Hard%", "Barrel%", "HardHit%")
+for i in per_cols:
+    pitchers[i] = (pd.to_numeric(pitchers[i].str[:-1]).div(100).mask(pitchers[i] == '%', 0))
+
 # Generate a points column based on fantasy scoring (fangraphs doesn't record a quality starts)
-pitchers["points"] = (pitchers['IP']*3) + (pitchers['H']*-1) + (pitchers['ER']*-2) + (pitchers['BB']*-1) + (pitchers['HBP']*-1) + (pitchers['SO']*1) + (pitchers['W']*3) + (pitchers['L']*-3)
+pitchers["points"] = (pitchers['IP'] * 3) + (pitchers['H'] * -1) + (pitchers['ER'] * -2) + (pitchers['BB'] * -1) + (
+            pitchers['HBP'] * -1) + (pitchers['SO'] * 1) + (pitchers['W'] * 3) + (pitchers['L'] * -3)
 
 # Summary stats for each column
-pd.set_option('display.max_columns', None)
 #print(pitchers.describe())
 
+
+########################################################################################################################
+#################################### REGRESSION MODELS #################################################################
+########################################################################################################################
+
+# Function for during list of variables in dataframe with variable name & regression coefficient
+def reg_points_to_df(vars):
+    reg = linear_model.LinearRegression()
+    variables = (vars)
+    reg.fit(pitchers[variables], pitchers.points)
+    variables = pd.Series(vars)
+    reg_cos = pd.Series(reg.coef_)
+    df = pd.concat([variables, reg_cos], axis=1)
+    return (df)
+
 # Regression model with basic stats against points
-reg = linear_model.LinearRegression()
-basic_variables = ['W', "L", "ERA", "IP", "H", "TBF", 'ER', 'BB', "HR", 'SO', 'K/9', 'BB/9', 'K/BB', 'HR/9']
-reg.fit(pitchers[basic_variables], pitchers.points)
-basic_variables = pd.Series(['W', "L", "ERA", "IP", "H", "TBF", 'ER', 'BB', "HR", 'SO', 'K/9', 'BB/9', 'K/BB', 'HR/9'])
-reg_cos = pd.Series(reg.coef_)
-basic = pd.concat([basic_variables, reg_cos], axis=1)
-print(basic)
+basic = reg_points_to_df(['W', "L", "ERA", "IP", "H", "TBF", 'ER', 'BB', "HR", 'SO', 'K/9', 'BB/9', 'K/BB', 'HR/9'])
+# print(basic)
+
+# Regression model with advanced stats against points
+adv = reg_points_to_df(['xFIP', "BABIP"])
+# print(adv)
+
+# Regression model with batted ball stats against points
+bb = reg_points_to_df(['Soft%', 'Med%', 'Barrel%', 'HardHit%'])
+print(bb)
 
 
 '''
