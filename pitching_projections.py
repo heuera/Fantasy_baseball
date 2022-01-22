62d2da6e73ddc203ee247dd12ea79149e56f3173
heuera <austinheuer24@gmail.com>
2022-01-22 15:47:50 -0500

    Removed % sign from values in percentage variables. Started with exploratory analysis.

--- pitching_projections.py
+++ pitching_projections.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # FOR OUTPUT FORMAT PYCHARM
-pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
 
-# DATA CLEANING -------------------------------------------------------------------------------------------------
+# DATA CLEANING --------------------------------------------------------------------------------------------------------
 
 # Load in dataframes
 df_standard = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/data/Standard.csv")
@@ -23,18 +25,53 @@ df_pitchers.drop(['Name_y', 'Team_y', 'ERA_y', 'BABIP_y', 'FB%_y', 'Name_x', 'Te
 df_pitchers.rename(columns = {'ERA_x':'ERA', 'BABIP_x':'BABIP', 'FB%_x':'FB%'}, inplace = True)
 
 # Make a number of seasons variable
-df_pitchers['num_seasons'] = df_pitchers.sort_values('Season', ascending=True).groupby('playerid').cumcount()+1       # orders by year, groups by player, then counts number of rows for each player
-df_pitchers = df_pitchers[df_pitchers.IP > 59]                                                                        # filters out people with less than 60 IP (needs to use 0 min to start to make sure I got every year for number of season count)
-df_pitchers = df_pitchers[df_pitchers.Season > 1999]                                                                  # filters out everything before 2000
+df_pitchers['num_seasons'] = df_pitchers.sort_values('Season', ascending=True).groupby('playerid').cumcount()+1
+
+# Point column doesn't include quality starts because FanGraphs doesn't provide them
+df_pitchers["Points"] = (df_pitchers['IP'] * 3) + (df_pitchers['H'] * -1) + (df_pitchers['ER'] * -2) + \
+                        (df_pitchers['BB'] * -1) + (df_pitchers['HBP'] * -1) + (df_pitchers['SO'] * 1)
+
+# Create a lead_points column which shows how many points the player had the the next year
+df_pitchers['Points_lead'] = df_pitchers.sort_values(['playerid', 'Season']).groupby(['playerid'])['Points'].shift(-1)
+
+# Filter out people with less than 60 IP (use 0 min to start to make sure I include each year in count regardless of IP)
+df_pitchers = df_pitchers[df_pitchers.IP > 59]
+
+# Filter out everything before 2000
+df_pitchers = df_pitchers[df_pitchers.Season > 1999]
 
 # Multi-level index of  player and then each season for that player
-df_pitchers = df_pitchers.set_index(['playerid', 'Season'])
+#df_pitchers = df_pitchers.set_index(['playerid', 'Season'])
 
-# Point column doesnt include quality starts because FanGraphs doesn't provide them
-df_pitchers["Points"] = (df_pitchers['IP'] * 3) + (df_pitchers['H'] * -1) + (df_pitchers['ER'] * -2) + (df_pitchers['BB'] * -1) + (df_pitchers['HBP'] * -1) + (df_pitchers['SO'] * 1)
+# Get rid of % sign from all columns that are percentages and then divide by 100
+cols = df_pitchers.columns
+for head in cols:
+    if "%" in head:
+        df_pitchers[head] = (pd.to_numeric(df_pitchers[head].str[:-1]).div(100).mask(df_pitchers[head] == '%', 0))
 
-# need to create a lead_points column where value is number of points player scored in next season (ie current row + 1)
+# EXPLORATORY ANALYSIS -------------------------------------------------------------------------------------------------
 
-# EXPLORATORY ANALYSIS ------------------------------------------------------------------------------------------
+# Look at the distributions
+vars = ['ERA', 'CG', 'IP', 'TBF', 'H', 'R', 'ER', 'HR', 'BB', 'K/9', 'BB/9', 'K/BB', 'HR/9', 'FBv', 'SIERA', 'WHIP']
+for var in vars:
+    var = str(var)
+    plt.hist(df_pitchers[var], bins=50)
+    plt.xlabel(var)
+    plt.ylabel('Count')
+    plt.axvline(df_pitchers[var].mean(), color='k', linestyle='dashed', linewidth=1)
+    #plt.show()
+
+# Visualize variables by year
+sns.set(style='whitegrid')
+vars = ['W', 'ERA', 'IP', 'ER', 'HR', 'SO', 'K/9', 'BB/9', 'K/BB', 'HR/9', 'WHIP', 'BABIP', 'SIERA', 'Hard%', 'FBv']
+for var in vars:
+    var = str(var)
+    ax = sns.boxplot(x="Season", y=var, data=df_pitchers)
+    #plt.show()
+
+# WILL ADD TO EXPLORATORY ANALYSIS
+
+
+# PITCHER PERFORMANCE MODEL --------------------------------------------------------------------------------------------
 
 
