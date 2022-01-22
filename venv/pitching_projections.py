d188220df47da44217a37ad59f5bd7c88b67de08
heuera <austinheuer24@gmail.com>
2022-01-22 07:52:25 -0500

    Updated with FanGraphs data that includes year. Only included pitchers with > 60 IP in season

--- venv/pitching_projections.py
+++ venv/pitching_projections.py
@@ -10,10 +10,10 @@ df_standard = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/data/Stan
 df_advanced = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/data/Advanced.csv")
 df_batted = pd.read_csv("/Users/austinheuer/Desktop/Fantasy_baseball/data/Battedball.csv")
 
-
 # Merge the dataframes I'm interested in
-df_pitchers = pd.merge(df_standard, df_advanced, how='left', on=['ERA', 'playerid'])
-df_pitchers.drop(['Name_y', 'Team_y'], axis=1, inplace=True)
-df_pitchers = pd.merge(df_pitchers, df_batted, how='left', on=['BABIP', 'playerid'])
-df_pitchers.drop(['Name_x', 'Team_x'], axis=1, inplace=True)
+df_pitchers = pd.merge(df_standard, df_advanced, how='left', on=['playerId', 'Season'])
+df_pitchers = pd.merge(df_pitchers, df_batted, how='left', on=['playerId', 'Season'])
+df_pitchers.drop(['Name_y', 'Tm_x', 'TBF_y', 'AVG_y', 'IP_y', 'Name_x', 'Tm_y', 'TBF_x'], axis=1, inplace=True)
+df_pitchers.rename(columns = {'AVG_x':'AVG', 'IP_x':'IP'}, inplace = True)
 
+print(df_pitchers)
